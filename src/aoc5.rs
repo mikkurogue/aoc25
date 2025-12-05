167791c416c541c1b452bb5b6cbac64e5f2a2a0a
mikku <michael.lindemans@outlook.com>
2025-12-05 19:32:49 +0200

    aoc day 5 part one, im SO smart i can feel my iq increasing

---
+++ src/aoc5.rs
@@ -0,0 +1,80 @@
+use std::collections::btree_map::Values;
+
+#[derive(Debug)]
+struct Range {
+    start: i64,
+    end: i64,
+}
+
+pub fn solve() {
+    let instructions =
+        std::fs::read_to_string("input-aoc5.txt").expect("Failed to read input file");
+
+    let mut ranges: Vec<Range> = Vec::new();
+    let mut values_to_check: Vec<i64> = Vec::new();
+    instructions.trim_end().lines().for_each(|line| {
+        // Skip empty lines
+        if line.is_empty() {
+            return;
+        }
+
+        if line.contains('-') {
+            let parts: Vec<&str> = line.split('-').collect();
+            if parts.len() != 2 {
+                eprintln!("Invalid range format: {}", line);
+                return;
+            }
+
+            let start = parts[0].parse::<i64>();
+            let end = parts[1].parse::<i64>();
+
+            match (start, end) {
+                (Ok(s), Ok(e)) => {
+                    ranges.push(Range { start: s, end: e });
+                }
+                _ => {
+                    eprintln!("Failed to parse range: {}", line);
+                }
+            }
+
+            return;
+        }
+
+        // the values we want to check
+        if !line.contains('-') {
+            line.parse::<i64>()
+                .map(|value| values_to_check.push(value))
+                .unwrap_or_else(|_| {
+                    eprintln!("Failed to parse value to check: {}", line);
+                });
+
+            return;
+        }
+    });
+
+    let mut found_count = 0;
+
+    for value in &values_to_check {
+        let mut found = false;
+        for range in &ranges {
+            if is_value_in_range(*value, range) {
+                println!("Value {} is in range {:?}.", value, range);
+                found = true;
+                found_count += 1;
+                break;
+            }
+        }
+        if !found {
+            println!("Value {} is NOT in any range.", value);
+        }
+    }
+
+    // println!("Ranges: {:#?}", ranges);
+    // println!("Values to check: {:#?}", values_to_check);
+
+    println!("Fresh ingredients: {}", found_count);
+}
+
+fn is_value_in_range(value: i64, range: &Range) -> bool {
+    value >= range.start && value <= range.end
+}
--- src/main.rs
+++ src/main.rs
@@ -2,17 +2,20 @@ mod aoc1;
 mod aoc2;
 mod aoc3;
 mod aoc4;
+mod aoc5;
 
 fn main() {
-    aoc1::solve()
-        .map_err(|e| {
-            eprintln!("Error: {}", e);
-        })
-        .ok();
-
-    aoc2::solve();
-
-    aoc3::solve();
-
-    aoc4::solve();
+    // aoc1::solve()
+    //     .map_err(|e| {
+    //         eprintln!("Error: {}", e);
+    //     })
+    //     .ok();
+    //
+    // aoc2::solve();
+    //
+    // aoc3::solve();
+    //
+    // aoc4::solve();
+    //
+    aoc5::solve();
 }
